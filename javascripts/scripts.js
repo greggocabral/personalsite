f745d139ddbdedd57d86ef4d8476580eaac4ea45
greggocabral <cabral.gr@gmail.com>
2016-08-02 23:23:30 +0300

    subcategories

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -5,12 +5,22 @@ var slideSpeed = 600;
 $(document).ready(function() {
 
 	$(".category-text").hide();
+	$(".sub-category-text").hide();
 
 	$('#about-photo').hide().css('visibility', 'visible').fadeIn(1000);
 
 	$(".link-category" ).click(function() { 
 	 		
 	 		$(".category-text").slideUp(slideSpeed);
+	 		$(".sub-category-text").slideUp(slideSpeed);
+
+	 		$(this).next().slideDown(slideSpeed);
+	});	
+
+	$(".link-sub-category" ).click(function() { 
+	 		
+	 		
+	 		$(".sub-category-text").slideUp(slideSpeed);
 
 	 		$(this).next().slideDown(slideSpeed);
 	});	
