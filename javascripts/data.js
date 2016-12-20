ba3be5cc708ef94bb82cbca884fdd9d6852f9851
greggocabral <cabral.gr@gmail.com>
2016-12-20 01:42:08 -0300

    project description improvement

--- javascripts/data.js
+++ javascripts/data.js
@@ -9,36 +9,29 @@ var projects = [
 	{
 		'id': 'blumornadesayunoscom',
 		'title': 'Blumorna Desayunos',
-		'description': '<p>Single page static website, developed for a small argentinean company that delivers homemade breakfast.</p><p> The job involved helping the business owners to pick a name for the brand, designing their logo design, and building and hosting the website. </p><p> The technologies employed include Bootstrap, HTML and CSS. The logo design was done in Illustrator.</p>',
+		'description': '<p>Single page responsive static website, developed for a small argentinean company that delivers homemade breakfasts.</p><p> The job involved helping the business owners to pick a name for the brand, designing their logo and building and hosting the website. </p><p> The technologies employed included Bootstrap, HTML, CSS and jQuery. The logo design was done in Illustrator.</p>',
 		'link': 'blumornadesayunos.com',
 		'img': './img/blumornadesayunos.png'
 	},
 	{
 		'id': 'chrisburtonartcom',
 		'title': 'Chris Burton Art',
-		'description': 'The job involved....',
+		'description': "<p>Single page static website, developed for a german musician and photographer that decided to integrate and show all his work in one single site. </p> <p> The job consisted in designing and developing the website. </p><p> The technologies employed were HTML, CSS and jQuery, embedding soundcloud and pixie snippets to feature all of Chris' work hosted in those platforms. </p>",
 		'link': 'chrisburtonart.com',
 		'img': './img/chrisburtonart.png'
 	},
 	{
 		'id': 'plansandbreakfastcom',
 		'title': 'Plans & Breakfast',
-		'description': 'Personal project...',
+		'description': '<p>Web app currently in construction, meant to deal with a personal need of a basic calendar with a comfortable panoramic view of multiple months at the same time, with a continuous smooth scroll between weeks. </p> <p> Some of the technologies employed are HTML, CSS, jQuery, NodeJS and MongoDB. </p>',
 		'link': 'plansandbreakfast.com',
 		'img': './img/plansandbreakfast.png'
 	}, 
 	{
 		'id': 'gregoriocabralme',
-		'title': 'This website',
-		'description': '<p>Is a responsive static website developed using pure editing straight on HTML,CSS and jQuery.</p> <p>Wanted to to make a tidy minimalist way of displaying some of the projects I am working on.</p>',
-		'link': '',
-		'img': ''
-	}, 
-	{
-		'id': 'valparnet',
-		'title': 'Valpar',
-		'description': '<p>Web app to for efficient language learning.</p> <p>Not running yet.</p> ',
-		'link': '',
+		'title': 'Personal Website',
+		'description': '<p>Single page responsive static website, designed to display in an organized but minimalistic way, some of the projects I have been working on.</p> <p> Some of the technologies employed are HTML, CSS and jQuery. </p>  ',
+		'link': 'gregoriocabral.me',
 		'img': ''
 	}, 
 	{
@@ -51,7 +44,7 @@ var projects = [
 	{
 		'id': 'windturbinedesign',
 		'title': 'Structural design of composite wind turbine blades',
-		'description': '<p>It was my mechanical engineering master thesis project.</p>',
+		'description': '<p>As part of my mechanical engineering master thesis project, me and my thesis partner calculated a recommended material distribution ifor a wind turbine blade. Utilizing applied numerical methods, we simulated several loading conditions for various reinforcement configurations. We concluded in one feasible resistant structure, for which we provided the manufaturing blueprints and notes.</p>',
 		'link': '',
 		'img': ''
 	}, 
@@ -62,4 +55,18 @@ var projects = [
 		'link': '',
 		'img': ''
 	}, 
+	{
+		'id': 'happymondaytravel',
+		'title': 'Happy Monday Travel',
+		'description': '<p>Compilation of my best travel photographs.</p>',
+		'link': '',
+		'img': ''
+	}, 
+	{
+		'id': 'languagelearning',
+		'title': 'Language learning',
+		'description': '<p> Valpar.net is a time efficient language learning platform, targeting word prioritization, spaced repeticion method and fast feedback loops. </p> <p>Not developed yet.</p> ',
+		'link': '',
+		'img': ''
+	}
 ];
